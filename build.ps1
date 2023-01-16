ead360769cb9f941ac18c33b53969dcc74343233
Stefano Sinigardi <stesinigardi@hotmail.com>
2023-01-16 17:08:09 +0100

    move to github actions

--- build.ps1
+++
@@ -1,8 +0,0 @@
-#Remove-Item .\build -Force -Recurse
-New-Item .\build -itemtype directory -Force
-cd build
-#cmake .. -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake"
-cmake .. -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" 
-cmake --build .
-cd ..
-
